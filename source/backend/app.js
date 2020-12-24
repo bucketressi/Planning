5adfcb43536109dc53fbee99aa9f203d1c46c640
heeeun <01072417732w@gmail.com>
2020-12-24 11:20:01 +0900

    action test

--- source/backend/app.js
+++ source/backend/app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express();
 app.set('port', process.env.PORT || 3001);
 
+
 app.listen(app.get('port'), ()=>{
 	console.log('익스프레스 서버 시작. 포트 : '+app.get('port'));
 });
